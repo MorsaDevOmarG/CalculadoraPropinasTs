744e67f9e20ad9531dd46d05d1c120dcfeee0de4
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-11-18 14:43:58 -0600

    Agregando los CONTENEDORES PRINCIPALES

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,23 @@ function App() {
   return (
     <>
       <header className="bg-teal-400 py-5">
-        <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumos</h1>
+        <h1 className="text-center text-4xl font-black">
+          Calculadora de Propinas y Consumos
+        </h1>
       </header>
+
+      {/* Media Queries = md: , lg: , etc.... */}
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
 export default App
