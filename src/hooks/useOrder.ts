3cb99eb8e762cefc62f2f60766f76c3296e1dac2
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-11-20 14:53:13 -0600

    Agregar SIGNATURE a la funcion de agregar articulo con TS

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,11 +1,11 @@
 import  { useState } from "react";
-import type { OrderItem } from "../types";
+import type { MenuItem, OrderItem } from "../types";
 
 export default function useOrder() {
   const [order, setOrder] = useState<OrderItem[]>([]);
 
-  const addItem = () => {
-    console.log("Agregar item");
+  const addItem = (item: MenuItem) => {
+    console.log("Agregar item", item);
   };
 
   return {
