bf58882738b8bdf3ebc443f173427892e54d382a
Omar <omar.garcia@morsa.com.mx>
2025-11-25 14:32:34 -0600

    USECALLBACK

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { OrderItem } from '../types/index';
 import { formatCurrency } from '../helpers/index';
 
@@ -8,9 +8,14 @@ type OrderTotalsProps = {
 };
 
 export default function OrderTotals({ order, tip }: OrderTotalsProps) {
-  const subtotalAmount = useMemo(() => order.reduce((total, item) => total + item.price * item.quantity, 0), [order]);
-  const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order]);
-  const totalAmount = useMemo(() => subtotalAmount + tipAmount, [tip, order]);
+  // const subtotalAmount = useMemo(() => order.reduce((total, item) => total + item.price * item.quantity, 0), [order]);
+  // const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order]);
+  // const totalAmount = useMemo(() => subtotalAmount + tipAmount, [tip, order]);
+
+  // useMEMO vs useCALLBACK, Funcionan igual en este caso, solo cambia la sintaxis
+  const subtotalAmount = useCallback(() => order.reduce((total, item) => total + item.price * item.quantity, 0), [order]);
+  const tipAmount = useCallback(() => subtotalAmount() * tip, [tip, order]);
+  const totalAmount = useCallback(() => subtotalAmount() + tipAmount(), [tip, order]);
 
   return (
     <>
@@ -19,17 +24,21 @@ export default function OrderTotals({ order, tip }: OrderTotalsProps) {
 
         <p className="text-2xl my-5">
           Subtotal a pagar: {''}
-            <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
+          {/* <span className="font-bold">{formatCurrency(subtotalAmount)}</span> */}
+          {/* De esta forma sería si ejecutamos useCallback */}
+            <span className="font-bold">{formatCurrency(subtotalAmount())}</span>
         </p>  
 
         <p className="text-2xl my-5">
           Propina: {''}
-          <span className="font-bold">{formatCurrency(tipAmount)}</span>
+          {/* <span className="font-bold">{formatCurrency(tipAmount)}</span> */}
+          <span className="font-bold">{formatCurrency(tipAmount())}</span>
         </p>
 
         <p className="text-2xl my-5">
           Total a pagar: {''}
-          <span className="font-bold">{formatCurrency(totalAmount)}</span>
+          {/* <span className="font-bold">{formatCurrency(totalAmount)}</span> */}
+          <span className="font-bold">{formatCurrency(totalAmount())}</span>
         </p>
       </div>
 
